1e1a5669e41887af4a4234a9384c005cdc1a9667
subhashraaj <150801940+subhashraaj@users.noreply.github.com>
2024-01-08 20:08:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+function daysOfAYear(year){
+    if((year%400==0) || (year%100!=0) && (year%4==0)){
+        return 366;
+    }
+    else{
+        return 365;
+    }
+}
+
+console.log(daysOfAYear(2001));
+
